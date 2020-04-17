eba3f6f710ad43a60505a03cfb064fe82ce68044
Nadia Polikarpova <nadia.polikarpova@gmail.com>
2020-04-17 15:13:14 -0700

    update page

--- site.hs
+++ site.hs
@@ -52,7 +52,7 @@ main = hakyll $ do
   match (fromList tops)    $ crunchWithCtx siteCtx
   match "lectures/00-*"    $ crunchWithCtxCustom "final" postCtx
   match "lectures/01-*"    $ crunchWithCtxCustom "final" postCtx
-  match "lectures/02-*"    $ crunchWithCtxCustom "lecture" postCtx
+  match "lectures/02-*"    $ crunchWithCtxCustom "final" postCtx
   -- match "lectures/03-*"    $ crunchWithCtxCustom "final" postCtx
   -- match "lectures/04-*"    $ crunchWithCtxCustom "final" postCtx
   -- match "lectures/05-*"    $ crunchWithCtxCustom "final" postCtx
